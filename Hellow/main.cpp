21c9945d5f221ef119e0d939bc9c564d96354830
Samy Driss <samydriss@Samys-MacBook-Pro.local>
2013-02-10 19:27:25 +0000

    Initial Commit

---
+++ Hellow/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hellow
+//
+//  Created by Samy Driss on 10/02/2013.
+//  Copyright (c) 2013 Samy Driss. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
